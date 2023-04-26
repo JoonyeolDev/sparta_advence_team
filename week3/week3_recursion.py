394872483866b56263121b50340a77c62aecff2c
Yoon joonyeol <raoneli@naver.com>
2023-04-26 19:56:12 +0900

    week3 commit

--- week3/week3_recursion.py
+++ week3/week3_recursion.py
@@ -10,19 +10,21 @@
 # import sys
 # print(sys.getrecursionlimit())
 
-# 
+# 팩토리얼
 def factorial(n):
     if n<=0: return 1
     return n*factorial(n-1)
 
 print(factorial(16)) # 20922789888000
 
+# 피보나치
 def fibonacci(a,b,n,count=2):
     if count == n: return b
     return fibonacci(b,a+b,n,count+1)
 
 print(fibonacci(0,1,12))
 
+# 파스칼
 def pascal(n, a=[1]):
     if len(a)==n: return a
     return pascal(n, [1]+[a[i-1]+a[i] for i in range(1,len(a))]+[1])
@@ -30,5 +32,3 @@ def pascal(n, a=[1]):
 for i in range(1,8):
     print(pascal(i))
 
-# a = [1]
-# [a[i-1]+a[i] for i in range(len(a[1:]))]
